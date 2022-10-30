70e32b1d7311ae328921f6828826ba09fe3bc357
Vlad Losev <Vladius1744@yandex.ru>
2022-10-30 13:32:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// 'use strict';
+
+let NameProduct = 'bananas';
+let QuantityGoods = 5;
+let ProductCategory = 'FoodProduct';
+let PriceProduct = 20;
+
+console.log('NameProduct:','bananas');
+
+console.log(PriceProduct * QuantityGoods);
+
+console.log(20 * 5);
